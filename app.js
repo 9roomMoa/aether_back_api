8824706d1b34230ef6c408a112edd073ccd99357
Lims <swoo0514lim@gmail.com>
2025-01-08 21:50:57 +0900

    [CHORE]: cors 주석처리

--- app.js
+++ app.js
@@ -14,19 +14,19 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 // 보안 관련 미들웨어 및 로깅 설정 (production 환경에 따른 설정)
-if (process.env.NODE_ENV === 'production') {
-  app.enable('trust proxy');
-  app.use(morgan('combined')); // 생산 환경에서는 'combined'로 로깅
-  app.use(
-    helmet({
-      contentSecurityPolicy: false,
-      crossOriginEmbedderPolicy: false,
-      crossOriginResourcePolicy: false,
-    })
-  );
-} else {
-  app.use(morgan('dev')); // 개발 환경에서는 'dev'로 로깅
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.enable('trust proxy');
+//   app.use(morgan('combined')); // 생산 환경에서는 'combined'로 로깅
+//   app.use(
+//     helmet({
+//       contentSecurityPolicy: false,
+//       crossOriginEmbedderPolicy: false,
+//       crossOriginResourcePolicy: false,
+//     })
+//   );
+// } else {
+//   app.use(morgan('dev')); // 개발 환경에서는 'dev'로 로깅
+// }
 
 // CORS 설정
 // app.use(
@@ -41,18 +41,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // 세션 설정
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET || 'secret-key',
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//       secure: process.env.NODE_ENV === 'production', // production 환경에서는 secure 속성 설정
-//       httpOnly: true,
-//       maxAge: 1000 * 60 * 60, // 세션 만료 시간 설정 (1시간)
-//     },
-//   })
-// );
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'secret-key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production', // production 환경에서는 secure 속성 설정
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60, // 세션 만료 시간 설정 (1시간)
+    },
+  })
+);
 
 app.use('/', indexRouter); // 라우팅 처리
 
