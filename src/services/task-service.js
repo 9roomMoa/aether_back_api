3d569a45713451e2eb57dd3e4b9bd5a48e67b96a
Lims <swoo0514lim@gmail.com>
2025-01-29 22:15:29 +0900

    [REFACTOR]: comment -> task 리팩토링

--- src/services/task-service.js
+++ src/services/task-service.js
@@ -158,3 +158,88 @@ exports.searchComments = async (keyword, taskId, userId) => {
     throw new Error('error occured during searching comments');
   }
 };
+
+exports.createComment = async (commentData) => {
+  try {
+    if (commentData.parentId) {
+      const isChildComment = await Comment.findById(commentData.parentId);
+      if (!isChildComment) {
+        throw new Error('Comment does not have a parent comment');
+      }
+    }
+
+    const comment = new Comment(commentData);
+    await comment.save();
+
+    return comment;
+  } catch (err) {
+    console.error(err);
+    throw new Error('Error occured during creating comment');
+  }
+};
+
+exports.getComments = async (userId, taskId) => {
+  try {
+    const existingTask = await Task.findById(taskId);
+    if (!existingTask) {
+      throw new Error('No task found');
+    }
+
+    const isAvailable = await taskUtil.scopeChecker(userId, existingTask);
+    if (!isAvailable) {
+      throw new Error('No privilege to get the comments');
+    }
+
+    const comments = await Comment.find({ taskId })
+      .sort({ createdAt: 1 })
+      .lean();
+
+    const commentMap = {};
+
+    comments.forEach((comment) => {
+      comment.replies = [];
+      commentMap[comment._id] = comment;
+    });
+
+    const result = [];
+    comments.forEach((comment) => {
+      if (comment.parentId) {
+        if (commentMap[comment.parentId]) {
+          commentMap[comment.parentId].replies.push(comment);
+        }
+      } else {
+        result.push(comment);
+      }
+    });
+
+    return result;
+  } catch (err) {
+    console.error(err);
+    throw new Error('Error occured during getting comments');
+  }
+};
+
+exports.deleteComment = async (userId, taskId, commentId) => {
+  try {
+    const existingTask = await Task.findById(taskId);
+    if (!existingTask) {
+      throw new Error('Invalid task id');
+    }
+
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+      throw new Error('Invalid comment Id');
+    }
+    const isCreator = await taskUtil.isCommentCreator(userId, comment);
+
+    if (!isCreator) {
+      throw new Error('No privilege to delete this comment');
+    }
+
+    const result = await Comment.findByIdAndDelete(commentId);
+    return result;
+  } catch (err) {
+    console.error(err);
+    throw new Error('Error occured during deleting a comment');
+  }
+};
