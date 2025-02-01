926ba3650f86e32d143653be0e4296e629ad0635
Lims <swoo0514lim@gmail.com>
2025-02-01 21:29:23 +0900

    [REFACTOR]: task-validation-schema 이동

---
+++ src/validation/task-validation.js
@@ -0,0 +1,13 @@
+const Joi = require('joi');
+
+exports.taskValidationSchema = Joi.object({
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  status: Joi.string().valid('To Do', 'In Progress', 'Done').default('To Do'),
+  priority: Joi.number().integer().min(1).max(5),
+  project: Joi.string().required(), // 프로젝트 ID
+  assignedTo: Joi.array().items(Joi.string()).optional(), // 사용자 ID
+  createdBy: Joi.string().required(), // 생성자 ID
+  startDate: Joi.date().optional(),
+  dueDate: Joi.date().optional(),
+});
