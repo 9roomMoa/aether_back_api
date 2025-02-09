9c624a2dba7369396badb7b6143b3542625ccdd9
Lims <swoo0514lim@gmail.com>
2025-02-09 11:44:34 +0900

    [REFACTOR]: 재분기

--- src/routes/task-route.js
+++ src/routes/task-route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const taskController = require('../controllers/task-controller');
 const docsController = require('../controllers/docs-controller');
 const uploadMiddleware = require('../middlewares/upload');
+const commentController = require('../controllers/comment-controller');
 
 router.post('/', taskController.createTask);
 
@@ -15,15 +16,15 @@ router.get('/:tid/info', taskController.getTaskInfo);
 
 router.patch('/:tid/info', taskController.updateTaskInfo);
 
-router.post('/:tid/comments', taskController.createComment);
+router.post('/:tid/comments', commentController.createComment);
 
-router.get('/:tid/comments', taskController.getComments);
+router.get('/:tid/comments', commentController.getComments);
 
-router.patch('/:tid/comments/:cid', taskController.updateComment);
+router.patch('/:tid/comments/:cid', commentController.updateComment);
 
-router.delete('/:tid/comments', taskController.deleteComment);
+router.delete('/:tid/comments', commentController.deleteComment);
 
-router.get('/:tid/comments/search', taskController.searchComments);
+router.get('/:tid/comments/search', commentController.searchComments);
 
 router.get('/:tid/managers', taskController.getManagerInfo);
 
