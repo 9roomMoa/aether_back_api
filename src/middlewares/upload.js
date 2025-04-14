95b230cefdccc6b79c2dfb723b49c8c1afab0118
Lims <swoo0514lim@gmail.com>
2025-04-14 22:11:26 +0900

    [REFACTOR]: 파일 용량 제한

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -2,7 +2,10 @@ const multer = require('multer');
 
 // ✅ 메모리 스토리지 사용 (GridFS 업로드용)
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: { fileSize: 1024 * 1024 * 20 },
+});
 
 // ✅ 단일 파일 업로드 설정 (필드명: 'file')
 module.exports = {
